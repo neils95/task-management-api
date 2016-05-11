68fbc57b3e9f48a17d98a1be0ea422c8823c5a51
Neil Sanghrajka <neilsanghrajka@outlook.com>
2016-05-11 12:22:14 -0700

    Modified server.js landing message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app =express();
 var PORT =process.env.PORT||3000;
 
 app.get('/',function(req,res){
-	res.send('Todo API Roots');
+	res.send('Todo API Root');
 });
 app.listen(PORT,function(){
 	console.log('Express listening on port number ' + PORT+'!');
