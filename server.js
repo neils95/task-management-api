283cebb830d7e218abcda580c61d10935f326e98
Neil Sanghrajka <neilsanghrajka@outlook.com>
2016-05-12 17:24:50 -0700

    Updated DELETE

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.delete('/todos/:id', function(req, res) {
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted>1) {
 			todo.destroy();
-			return res.json(todo.toJSON());
+			return res.json(todo.status(204));
 		} else {
 			return res.status(404).json({
 				error:'No todo with id'
